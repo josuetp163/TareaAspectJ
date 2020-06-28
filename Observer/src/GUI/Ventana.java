adc92186d7298845e0bc7a36c53c0872a575209f
micxarce <micharcesierra2028@gmail.com>
2020-06-28 18:48:39 -0500

    Añadida nueva funcionalidad que agrega nuevos botones

--- Observer/src/GUI/Ventana.java
+++ Observer/src/GUI/Ventana.java
@@ -1,8 +1,13 @@
 package GUI;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -12,10 +17,17 @@ public class Ventana {
     private Scene scene;
     private BorderPane root;
     private PanePrincipal pp;
-    private HBox botones;
-
+    private FlowPane botones;
+    private Button btn;
+    
+    private Stage stageBotones;
+    private Scene sceneBotones;
+    private VBox rootBotones;
+    
+    
     //Constructor
     public Ventana(Stage stage) {
+    	crearEstrucutraBotones();
         crearEstructura(stage);
         stage.setTitle("Aspectos");
         stage.setScene(scene);
@@ -25,17 +37,19 @@ public class Ventana {
     //Crea la escena, la ventana y los agrega junto con los botones
     private void crearEstructura(Stage stage) {
         root = new BorderPane();
+        mostrarVentanaBotones();
         crearPanePrincipal();
         crearBotones();
         root.setCenter(pp);
         root.setBottom(botones);
+        root.setRight(btn);
         scene = new Scene(root,600,600);
         stage.setScene(scene);
     }
 
     // Método encargado de crear los botones y colocarlos en un HBox
     private void crearBotones() {
-    	botones = new HBox();
+    	botones = new FlowPane();
     	Boton bt1 = new Boton("blue",pp);
     	Boton bt2 = new Boton("yellow",pp);
     	Boton bt3 = new Boton("green",pp);
@@ -49,5 +63,49 @@ public class Ventana {
     
     public Scene getScene() {
         return scene;
+    }
+    
+    // Funcionalidad Nueva: Añadir nuevos botones
+    private void crearEstrucutraBotones() {
+    	crearVentanaBotones();
+    	sceneBotones = new Scene(rootBotones, 250, 250);
+    	stageBotones.setScene(sceneBotones);
+    }
+    
+    private void agregarBotones(String color) {
+    	Boton btn = new Boton(color,pp);
+    	botones.getChildren().addAll(btn.getButton());
+    }
+    
+    private void crearVentanaBotones() {
+    	rootBotones = new VBox();
+    	TextField txt = new TextField("Ingrese un color");
+    	Button btn = new Button("Agregar");
+    	btn.setOnAction(e -> {
+    		String color = txt.getText();
+			try {
+				Color c = Color.valueOf(color);
+				if (c != null) {
+					agregarBotones(color);
+					stageBotones.close();
+				}
+				else {
+					txt.setText("Ingrese un color válido");
+				}
+			}
+			catch(IllegalArgumentException ex) {
+				txt.setText("Ingrese un color válido");
+			}
+    	});
+    	rootBotones.getChildren().addAll(txt,btn);
+    }
+    
+    private void mostrarVentanaBotones() {
+    	btn = new Button("Nuevo Botón");
+    	btn.setOnAction(e -> {
+    		stageBotones.show();
+    		stageBotones.setAlwaysOnTop(true);
+    		stageBotones.centerOnScreen();
+    	});
     }
 }
